9e0eb5dcef40157884d38af49d1697ad647f7541
Harshit Mishra <harshitmishra.84162@gmail.com>
2025-11-30 22:18:45 +0530

    latest commit

--- app/app/calc.py
+++ app/app/calc.py
@@ -6,3 +6,6 @@ def add(x,y):
     """Returns the sum of x and y"""
     return x + y
     
+    
+def subtract(x,y):
+    return x-y
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,7 +1,12 @@
 from django.test import TestCase
-from .calc import add
+from .calc import add,subtract
 class CalcTestCase(TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
         
+    def test_sub(self):   # function must start with test_
+        """test subtact"""
         
+        res=subtract(12,8)
+        
+        self.assertEqual(res,4)
